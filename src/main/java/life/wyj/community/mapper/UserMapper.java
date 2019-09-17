1a2aca55e76e824645846090343d708418831b4a
uzuki555 <aaa314568649@qq.com>
2019-09-17 18:40:55 +0800

    9 17 18 40

--- src/main/java/life/wyj/community/mapper/UserMapper.java
+++ src/main/java/life/wyj/community/mapper/UserMapper.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper {
 
-    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified,avatar_url) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl})")
     void insert(User user);
 
     @Select("select id as id ,account_id as accountId ,name as name,token as token ,gmt_create as gmtCreate ,gmt_modified as gmtModified   from user where token=#{token}")
