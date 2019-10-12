4bd4e4ec9d0f70024996f9eac9d08b0f71ed5dd5
uzuki555 <aaa314568649@qq.com>
2019-10-12 18:42:17 +0800

    20191012 18:42

--- src/main/java/life/wyj/community/controller/FileController.java
+++ src/main/java/life/wyj/community/controller/FileController.java
@@ -2,18 +2,43 @@ package life.wyj.community.controller;
 
 import life.wyj.community.dto.FileDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class FileController {
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDTO upload(){
+    public FileDTO upload(@RequestParam("fileName") MultipartFile file){
+        if(file.isEmpty()){
+            return null;
+        }
+        String fileName = file.getOriginalFilename();
+        String[] filePaths = fileName.split(".");
+        String generatedFileName = UUID.randomUUID().toString()+"."+filePaths[filePaths.length-1];
+        long size = file.getSize();
+        try {
+            File path = new File(ResourceUtils.getURL("classpath:").getPath());
+            File dest = new File(path.getAbsolutePath(),"static/uploadImage/"+generatedFileName);
+            file.transferTo(dest);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
-        fileDTO.setMessage();
-        fileDTO.setUrl();
+        fileDTO.setUrl("/uploadImage/"+generatedFileName);
         return  new FileDTO();
     }
 }
