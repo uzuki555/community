dd87d61a48fe7263477c61283f91d683a3b78adb
uzuki555 <aaa314568649@qq.com>
2019-10-15 18:33:19 +0800

    10/15 18:33

--- src/main/java/life/wyj/community/controller/IndexController.java
+++ src/main/java/life/wyj/community/controller/IndexController.java
@@ -20,9 +20,11 @@ public class IndexController {
                          Model model,
                          @RequestParam(name = "page",defaultValue = "1") Integer page,
                          @RequestParam(name = "size",defaultValue = "5") Integer size ,
-                         @RequestParam(name = "search",required = false) String search){
+                         @RequestParam(name = "search",required = false ) String search){
 
         PaginationDTO pagination = questionService.list(search,page,size);
+
+
         model.addAttribute("pagination",pagination);
         model.addAttribute("search",search);
         return "index";
